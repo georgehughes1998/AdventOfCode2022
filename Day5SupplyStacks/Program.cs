6b485dd673ab5bda1612b152fdd98e0ccc8a8aef
RENISHAW\gh151040 <george.hughes@renishaw.com>
2022-12-07 16:21:26 +0000

    completed part one

--- Day5SupplyStacks/Program.cs
+++ Day5SupplyStacks/Program.cs
@@ -7,7 +7,10 @@ public class Program
 {
    public static void Main(string[] args)
    {
-      Console.WriteLine("Hello World.");
+      Console.WriteLine(Reader
+         .ReadCrateStacks(args[0])
+         .Rearrange(Reader.ReadInstructions(args[0]))
+         .GetTops());
    }
 }
 
@@ -15,52 +18,105 @@ public static class SupplyStacksTests
 {
    private const string TestDataFilePath = "..\\..\\..\\TestData.txt";
 
+   private static readonly IEnumerable<Instruction> TestDataInstructions = new List<Instruction>
+   {
+      new(1, 2, 1),
+      new(3, 1, 3),
+      new(2, 2, 1),
+      new(1, 1, 2)
+   };
+
+   private static readonly Func<IList<CrateStack>> TestDataCrateStacksCreator = () => new List<CrateStack>
+   {
+      new CrateStack()
+         .Push(new Crate('Z'))
+         .Push(new Crate('N')),
+      new CrateStack()
+         .Push(new Crate('M'))
+         .Push(new Crate('C'))
+         .Push(new Crate('D')),
+      new CrateStack()
+         .Push(new Crate('P'))
+   };
+
    [Test]
    public static void ReadInstructions_WithTestData_ReturnsExpectedValues()
    {
-      IEnumerable<Instruction> expectedInstructions = new List<Instruction>
-      {
-         new(1, 2, 1),
-         new(3, 1, 3),
-         new(2, 2, 1),
-         new(1, 1, 2)
-      };
-
-      Assert.AreEqual(expectedInstructions, Reader.ReadInstructions(TestDataFilePath));
+      Assert.AreEqual(TestDataInstructions, Reader.ReadInstructions(TestDataFilePath));
    }
 
    [Test]
    public static void ReadCrateStacks_WithTestData_ReturnsExpectedValues()
    {
-      IEnumerable<CrateStack?> expectedCrateStacks = new List<CrateStack?>
+      Assert.AreEqual(TestDataCrateStacksCreator(), Reader.ReadCrateStacks(TestDataFilePath));
+   }
+
+   [Test]
+   public static void Rearrange_WithTestData_ReturnsExpectedValues()
+   {
+      IList<CrateStack> expectedResultStack = new List<CrateStack>
       {
          new CrateStack()
-            .Push(new Crate('Z'))
-            .Push(new Crate('N')),
+            .Push(new Crate('C')),
          new CrateStack()
-            .Push(new Crate('M'))
-            .Push(new Crate('C'))
-            .Push(new Crate('D')),
+            .Push(new Crate('M')),
          new CrateStack()
             .Push(new Crate('P'))
+            .Push(new Crate('D'))
+            .Push(new Crate('N'))
+            .Push(new Crate('Z'))
       };
 
-      Assert.AreEqual(expectedCrateStacks, Reader.ReadCrateStacks(TestDataFilePath));
+      var testDataCrateStacks = TestDataCrateStacksCreator();
+
+      testDataCrateStacks.Rearrange(TestDataInstructions);
+      Assert.AreEqual(expectedResultStack, testDataCrateStacks);
+   }
+
+   [Test]
+   public static void ToString_WithTestCrateStacks_ReturnsExpectedValues()
+   {
+      Assert.AreEqual("NDP", TestDataCrateStacksCreator().GetTops());
    }
 }
 
-public static class Reader
+public static class CrateStackExtensions
 {
-   private static readonly Regex CrateRegex = new(@"(?<!\S) (?<char> ) (?!\S)|\[(?<char>\w)\]");
-   private static readonly Regex InstructionRegex = new(@"^move (?<move>\d+) from (?<from>\d+) to (?<to>\d+)$");
+   public static IList<CrateStack> Rearrange(this IList<CrateStack> stacks, Instruction instruction)
+   {
+      for (var i = 0; i < instruction.Count; i++) stacks[instruction.To - 1].Push(stacks[instruction.From - 1].Pop());
+      return stacks;
+   }
+
+   public static IList<CrateStack> Rearrange(this IList<CrateStack> stacks, IEnumerable<Instruction> instructions)
+   {
+      instructions.ToList().ForEach(instruction => stacks.Rearrange(instruction));
+      return stacks;
+   }
 
    public static CrateStack Push(this CrateStack self, CrateStack stack)
    {
       return stack.Count > 0 ? self.Push(stack.Pop()) : self;
    }
 
-   public static IEnumerable<CrateStack> ReadCrateStacks(string filename)
+   public static string GetTops(this IList<CrateStack> stacks)
    {
+      return stacks.Select(s => s.Peek().Identifier).Aggregate(string.Empty, (s1, s2) => s1 + s2);
+   }
+}
+
+public static class Reader
+{
+   private static readonly Regex CrateRegex = new(@" (?<char> ) (?: |$)|\[(?<char>\w)\](?: |$)");
+   private static readonly Regex InstructionRegex = new(@"^move (?<move>\d+) from (?<from>\d+) to (?<to>\d+)$");
+
+   public static IList<CrateStack> ReadCrateStacks(string filename)
+   {
+      var a = File.ReadLines(filename)
+         .Where(line => CrateRegex.IsMatch(line))
+         .Select(line =>
+            CrateRegex.Matches(line));
+
       return File.ReadLines(filename)
          .Where(line => CrateRegex.IsMatch(line))
          .Select(line =>
@@ -71,7 +127,8 @@ public static class Reader
          .Aggregate(
             (firstRow, secondRow) =>
                firstRow.Zip(secondRow)
-                  .Select(tuple => tuple.First.Push(tuple.Second)));
+                  .Select(tuple => tuple.First.Push(tuple.Second)))
+         .ToList();
    }
 
    public static IEnumerable<Instruction> ReadInstructions(string filename)
@@ -88,11 +145,6 @@ public static class Reader
 
 public class CrateStack : Stack<Crate>
 {
-   public override string ToString()
-   {
-      return Peek().ToString();
-   }
-
    public new CrateStack Push(Crate crate)
    {
       base.Push(crate);
@@ -102,17 +154,12 @@ public class CrateStack : Stack<Crate>
 
 public class Crate
 {
-   private readonly char _identifier;
-
    public Crate(char identifier)
    {
-      _identifier = identifier;
+      Identifier = identifier;
    }
 
-   public override string ToString()
-   {
-      return _identifier.ToString();
-   }
+   public char Identifier { get; }
 
    public override bool Equals(object? obj)
    {
@@ -122,12 +169,12 @@ public class Crate
    public bool Equals(Crate? other)
    {
       return other != null &&
-             _identifier == other._identifier;
+             Identifier == other.Identifier;
    }
 
    public override int GetHashCode()
    {
-      return _identifier.GetHashCode();
+      return Identifier.GetHashCode();
    }
 }
 
